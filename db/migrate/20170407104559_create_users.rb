863825a1eb8a59f15a57307630a7c9a419de4917
robot269006 <robot269006@yahoo.co.jp>
2017-04-08 12:38:43 +0900

    Create Users table
    1. Edit migration file (group_id commented out)
    2. bundle exec rake db:migrate

--- db/migrate/20170407104559_create_users.rb
+++ db/migrate/20170407104559_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.text        :name, null: false
+      t.text        :body
+      t.text        :email, null: false
+      t.text        :password, null:false
+      # t.integer     :group_id
       t.timestamps
     end
   end
