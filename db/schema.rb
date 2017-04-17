7cc419ffdb300179e32a276c90f09bbe9950f18d
robot269006 <robot269006@yahoo.co.jp>
2017-04-17 23:51:19 +0900

    Revise group_user table
    rails g migration RemoveColFromGroupuser
    -> Correct mistake of making “group_id_id” trying to add reference and
    foreign_key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417110700) do
+ActiveRecord::Schema.define(version: 20170417141233) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",    null: false
