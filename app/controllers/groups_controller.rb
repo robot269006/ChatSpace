ac057573f5823589365a83a4c45bc116d27060b3
robot269006 <robot269006@yahoo.co.jp>
2017-04-19 21:16:19 +0900

    Revision (edit render)
    render action: :new -> render :new
    This makes it not go through the controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,10 +10,10 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(create_params)
     if @group.save
-      redirect_to root_path(@group), notice: "チャットグループが作成されました"
+      redirect_to root_path, notice: "チャットグループが作成されました"
     else
       flash[:alert]="チャットグループの作成に失敗しました"
-      render action: :new
+      render :new
     end
   end
 
