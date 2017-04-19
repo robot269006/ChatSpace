a995537fcf807650cc750b5f608793df67ef5d84
robot269006 <robot269006@yahoo.co.jp>
2017-04-19 22:05:01 +0900

    Revision (Rename group_user columns, delete attribute nesting on user.rb)
    1. Rename group_user columns
    2. Delete attribute nesting on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :groups, through: :group_user
   has_many :group_users
-  accepts_nested_attributes_for :groups
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
