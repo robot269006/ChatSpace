688ad0fda6a3656572110d9b29ad944c818c722a
robot269006 <robot269006@yahoo.co.jp>
2017-04-21 22:26:38 +0900

    Enable checkbox and user addition
    1. Edit groups_controller
    2. Enable user addition when creating a group
    3. Enable user addition when editing a group
    
    *Unable to configure collection_check_box

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,7 +30,7 @@ class GroupsController < ApplicationController
 
   private
   def create_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, {:user_ids => []})
   end
 
 end
