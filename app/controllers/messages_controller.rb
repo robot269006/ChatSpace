6ff1b78b60f1aed9f281f0081b6393a11c56c983
robot269006 <robot269006@yahoo.co.jp>
2017-04-22 19:35:18 +0900

    Set controller
    1. Send messages instance to index method
    2. Send message instance to create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,16 @@
 class MessagesController < ApplicationController
 
 def index
+  @messages = Messages.all
 end
 
 def create
+  @message = Messages.new(create_params)
+end
+
+private
+def create_params
+  message.require(messages).permit(:body, :user_id, :group_id)
 end
 
 end
