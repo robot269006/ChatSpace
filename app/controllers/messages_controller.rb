b4aa83f0af565ab2eca98b7a6ae5fe9e4e7b5bc7
robot269006 <robot269006@yahoo.co.jp>
2017-04-26 21:03:18 +0900

    Set instances
    1. Try handing over @user instances first
    2. Try handing over @group instances
    
    *Error in @group = Group.find(params[:group_id])
    -> it works if “find(3)” is put in. So it must be something with
    association?

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :group_params, only: [:index, :create]
 
-def index
-end
+  def index
+    @user = User.find(current_user.id)
+    @group = Group.includes(:user)
+  end
 
-def create
-  @message = Messages.new(create_params)
-end
+  def create
+    @message = Messages.new(create_params)
+  end
 
-private
-def create_params
-  message.require(messages).permit(:body, :user_id, :group_id)
-end
+  private
+  def create_params
+    params.require(:messages).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+  def group_params
+    @group = Group.find(params[:groups_id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources 'groups'
-  resources 'messages', only: [:index, :create]
+  resources :groups do
+    resources :messages, only: [:index, :create]
+  end
 end
