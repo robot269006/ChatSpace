5435a011109cf33b538b40c4db35a15e2f3cedcd
robot269006 <robot269006@yahoo.co.jp>
2017-04-30 22:44:39 +0900

    Delete @user instance
    @user instance replaced with current_user

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
   before_action :group_params, only: [:index, :create]
 
   def index
-    @user = current_user
     @groups = current_user.groups
     @message = Message.new
   end
