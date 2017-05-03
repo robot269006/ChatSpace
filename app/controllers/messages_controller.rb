d21d6663115d7a7b41d868725eeb3c4c44acf347
robot269006 <robot269006@yahoo.co.jp>
2017-05-03 19:41:36 +0900

    Revision (each -> render)
    1. Delete “each” description from rendered files
    2. Create @messages instance in order to pass message info
    3. Allow rendering short-cut with “render @groups” and “render
    @messages”
    4. Rename rendered files to _group.html.haml and _message.html.haml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,9 @@ class MessagesController < ApplicationController
   before_action :set_group, only: [:index, :create]
 
   def index
-    @groups = current_user.groups
+    @groups = current_user.groups.includes(:users)
     @message = Message.new
+    @messages = Message.where(group_id: params[:group_id]).order('created_at ASC').includes(:user)
   end
 
   def new; end
