346c2f0d8482f52dd211babba00ae0275f4cc3de
robot269006 <robot269006@yahoo.co.jp>
2017-05-06 16:43:46 +0900

    Revision (factory-girl)
    delete user&group_id -> it’s auto-generated anyway

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :message do
     body       { Faker::Lorem.sentence }
-    user_id    1
-    group_id   1
     created_at { Faker::Time.backward(3, :evening)}
     updated_at { Faker::Time.backward(3, :evening)}
   end
