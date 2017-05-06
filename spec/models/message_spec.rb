8231dc55203bf36a1b3175e10270c31c8593dbcc
robot269006 <robot269006@yahoo.co.jp>
2017-05-06 15:35:45 +0900

    Revision (let)
    1. let used to set-up :message in the second test as well

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     let(:message) { build(:message) }
-
     it 'is valid with body, user_id, group_id, created_at and updated_at' do
       message.valid?
     end
 
+    let(:message) { build(:message, body: "" ) }
     it 'is invalid with a blank body' do
-      message = build(:message, body: "")
       message.valid?
       expect(message.errors[:body]).to include("can't be blank")
     end
