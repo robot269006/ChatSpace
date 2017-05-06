1882957031b6d8fd18ecff6a61beb19e5331c9dc
robot269006 <robot269006@yahoo.co.jp>
2017-05-06 17:10:03 +0900

    Revert "Revision (factory-girl)"
    This reverts commit 346c2f0d8482f52dd211babba00ae0275f4cc3de.

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :message do
     body       { Faker::Lorem.sentence }
+    user_id    1
+    group_id   1
     created_at { Faker::Time.backward(3, :evening)}
     updated_at { Faker::Time.backward(3, :evening)}
   end
