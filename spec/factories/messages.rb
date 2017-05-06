314f7e427bef9eb1b0bfb7ea2a1865028b75c29c
robot269006 <robot269006@yahoo.co.jp>
2017-05-06 14:13:14 +0900

    Model test (factory-girl settings)
    1. Factorygirl settings

---
+++ spec/factories/messages.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :message do
+    body       "AAAAAAA"
+    user_id    1
+    group_id   1
+    created_at "2017-01-01 10:00"
+    updated_at "2017-01-01 12:00"
+  end
+end
