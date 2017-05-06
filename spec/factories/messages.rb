19cd7a8d08260deb812a713f84e8e740ff4d7577
robot269006 <robot269006@yahoo.co.jp>
2017-05-06 14:41:14 +0900

    Model test (faker)
    1. Faker description to body, created and updated dates

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :message do
-    body       "AAAAAAA"
+    body       { Faker::Lorem.sentence }
     user_id    1
     group_id   1
-    created_at "2017-01-01 10:00"
-    updated_at "2017-01-01 12:00"
+    created_at { Faker::Time.backward(3, :evening)}
+    updated_at { Faker::Time.backward(3, :evening)}
   end
 end
