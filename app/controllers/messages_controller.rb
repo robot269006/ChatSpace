e18c6c2af6635be9321ccdcbe0a37034b1abbfc2
robot269006 <robot269006@yahoo.co.jp>
2017-05-06 19:30:39 +0900

    Pre-settings
    1. Create controller_spec file
    2. Comment out before_action on controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_group, only: [:index, :create]
+  # before_action :set_group, only: [:index, :create]
 
   def index
     @groups = current_user.groups.includes(:users)
