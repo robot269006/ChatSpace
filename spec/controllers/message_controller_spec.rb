9266beae15a61bf7037a74d8d954dbfe0b047185
robot269006 <robot269006@yahoo.co.jp>
2017-05-07 18:03:57 +0900

    Enable user login (devise and macros setting)
    1. Add config with devise and controller macros
    2. Controller macros setting
    3. Test “login_user” method defined in macros
    4. Password settings as login_user method showed error indicating that
    the password is not set

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,2 +1,10 @@
 require 'rails_helper'
 
+describe MessagesController do
+  login_user
+  describe 'GET #index' do
+    it 'renders index correctly' do
+    end
+  end
+
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
+  pass = Faker::Internet.password(8)
   factory :user do
     email  {Faker::Internet.email }
     name   {Faker::Name.first_name }
+    password pass
+    password_confirmation pass
   end
 end
