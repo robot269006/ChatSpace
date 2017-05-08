7965e074ee4c2105d2a03e74c492e85a9b826076
robot269006 <robot269006@yahoo.co.jp>
2017-05-08 22:44:12 +0900

    Controller Test (GET #index instances)
    1. Create list of factory instances by “create_list” method
    2. Use “sort” method to sort the instances order within the messages
    array

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -8,6 +8,12 @@ describe MessagesController do
     login_user
 
     describe 'GET #index' do
+      it 'assigns @messages instance correctly' do
+        messages = create_list(:message, 5, group_id: group.id)
+        get :index, params: { group_id: group.id}
+        expect(assigns(:messages)).to match(messages.sort{ |a, b| a.created_at <=> b.created_at })
+      end
+
       it 'renders index correctly' do
         get :index, params: { group_id: group }
         expect(response).to render_template :index
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email  {Faker::Internet.email }
-    name   {Faker::Name.first_name }
+    email    {Faker::Internet.email }
+    name     {Faker::Name.first_name }
     password {Faker::Internet.password(8)}
   end
 end
