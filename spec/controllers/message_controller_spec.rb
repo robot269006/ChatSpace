19a3c12b8552cda06246f0b990080975ac45058c
robot269006 <robot269006@yahoo.co.jp>
2017-05-08 20:37:10 +0900

    Controller Test (GET #index render)
    1. Fix password settings -> it caused NameError when running rake
    routes, since it was coded: pass = Faker::etc
    2. Test render -> NoMethod Error, saying that rails-controller-testing
    gem has to be installed
    3. Add rails-controller-testing to gemfile, and install
    4. Add group, message, user instances

--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
 describe MessagesController do
-  login_user
-  describe 'GET #index' do
-    it 'renders index correctly' do
-    end
-  end
+  context 'user signed in' do
+    let(:group)   { create(:group) }
+    let(:message) { create(:message) }
+    let(:user)    { create(:user) }
+    login_user
 
+    describe 'GET #index' do
+      it 'renders index correctly' do
+        get :index, params: { group_id: group }
+        expect(response).to render_template :index
+      end
+    end
+
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,7 @@
 FactoryGirl.define do
-  pass = Faker::Internet.password(8)
   factory :user do
     email  {Faker::Internet.email }
     name   {Faker::Name.first_name }
-    password pass
-    password_confirmation pass
+    password {Faker::Internet.password(8)}
   end
 end
