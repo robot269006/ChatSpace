6b442f7c51e090a98ddc73115cd9baad5350fede
robot269006 <robot269006@yahoo.co.jp>
2017-05-09 23:00:03 +0900

    Controller Test (POST #create render and instances)
    1. Apply “Context” to describe save success and failure
    2. Set redirect (and fixed render settings in /groups/index)
    3. Set rendering when saving message fails
    4. Set instance test -> only needs testing for Message.new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: "メッセージが作成されました"
+      redirect_to root_path(@group), notice: "メッセージが作成されました"
     else
       flash.now[:alert]="メッセージの作成に失敗しました"
       render :index
--- spec/controllers/message_controller_spec.rb
+++ spec/controllers/message_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 describe MessagesController do
   context 'user signed in' do
+    login_user
     let(:group)   { create(:group) }
     let(:message) { create(:message) }
     let(:user)    { create(:user) }
-    login_user
+    let(:message_params) do
+      { message: attributes_for(:message), group_id: group, user_id: user }
+    end
 
     describe 'GET #index' do
       it 'assigns @messages instance correctly' do
@@ -17,6 +20,26 @@ describe MessagesController do
       it 'renders index correctly' do
         get :index, params: { group_id: group }
         expect(response).to render_template :index
+      end
+    end
+
+    describe 'POST #create' do
+      context 'message saving succeeds' do
+        it 'saves the messages with @message instance' do
+          expect{post :create, params: message_params}.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group messages path' do
+          post :create, params: message_params
+          expect(response).to redirect_to root_path(group)
+        end
+      end
+
+      context 'message saving fails' do
+        it 'renders index when message saving fails' do
+          get :index, params: {group_id: group.id}
+          expect(response).to render_template :index
+        end
       end
     end
 
