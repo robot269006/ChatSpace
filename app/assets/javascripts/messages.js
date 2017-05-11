631d9640ccb6496733eaf6bdc09e0dd979aae3a4
robot269006 <robot269006@yahoo.co.jp>
2017-05-11 21:10:56 +0900

    Ajax settings
    1. Set ajax type, data
    2. Set success -> test with console.log
    3. Set failure -> test with console.log
    4. Set ajax url -> var postUrl = location.href;
    
    *limit displayed messages to 5 as it messes up view
    *small mistake noted in _group.haml so fixed it

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,20 @@
 $(function() {
   $('.write').on('submit', function(e){
     e.preventDefault();
-    message = $('.write__text').val();
-    console.log(message);
+    var textField = $('.write__text');
+    var message = textField.val();
+    var postUrl = location.href;
+    $.ajax({
+      type: 'POST',
+      url: postUrl + '.json',
+      data: { message: { body: message } },
+      dataType: 'json'
+    })
+    .done(function(data){
+      alert('success');
+    })
+    .fail(function(){
+      alert('error');
+    });
   });
 });
