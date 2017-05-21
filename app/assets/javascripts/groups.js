238f6ae3c4606b8a32493b68797316e8c1c23ba9
robot269006 <robot269006@yahoo.co.jp>
2017-05-21 14:55:20 +0900

    Revision (Small fixes, comments)
    1. Sanitization refactoring
    2. Add comments

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
+  //function that generates div that contains user name
   function buildHTML(user) {
     var html = '<div>' + user.name + '</div>'
     return html;
   }
+  //preset all the variables, to prevent "undefined" values
   var preSet = null;
   var preInput = "";
   var input = "";
   var result = "";
+  //XSS -> prevent malicious parameters being sent from the input
   sanitize = function(target){
     target.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
     return target;
   }
+  //set search function, that is later used together with setTimeout method
   ajaxSearch = function () {
     $.ajax({
-      // this hands over keyqord=xxx query data in json format to users/search path
+      // send request to /users/search(controller), with keyword+input parameters, with response in json format
       url: '/users/search',
       type: "GET",
       data: ("keyword=" + input),
@@ -30,9 +34,12 @@ $(document).ready(function() {
       alert('error');
     });
   };
+  //retrieve input from the search field
+  //run ajaxSearch in the right timing
   $('#search_window').on('keyup', function() {
     input = $.trim($(this).val());
     sanitize(input);
+    //receive keyup changes everytime, but only runs ajaxSearch every time input is changed, and input change is checked every 500ms
     if(preInput !== input){
       clearTimeout(preSet);
       preSet = setTimeout(ajaxSearch, 500);
--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,6 @@
 $(function() {
 
+  //append name, date, body to ul and li classes in _messages.haml partial files
   function buildHTML(datareceiver){
     var nameField = $('.user__name').val();
     var list = $('<ul class="individualmessage">');
@@ -8,12 +9,13 @@ $(function() {
     var body = list.append($('<li class="individualmessage__body">' + datareceiver.body + '<li>'));
     return list;
   };
-
-  sanitize = function(input){
-    input.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
-    return input;
+  //XSS -> prevent malicious parameters being sent
+  sanitize = function(target){
+    target.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+    return target;
   }
 
+  //retrieve typed in message
   $('.write').on('submit', function(e){
     e.preventDefault();
     var textField = $('.write__text');
@@ -26,6 +28,7 @@ $(function() {
       data: { message: { body: message } },
       dataType: 'json'
     })
+    //append resulting li elements to .group__middle class
     .done(function(data){
       var htmlChunk = buildHTML(data);
       $('.group__middle').append(htmlChunk);
